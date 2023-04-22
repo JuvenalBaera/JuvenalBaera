7eda38e141c4be65a3bdf3f1860bebd7a8d1409b
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2023-04-22 10:39:40 +0100

    Create a inputdata.py and make header of functions

---
+++ python/db_project/database/inputdata.py
@@ -0,0 +1,22 @@
+def inputPesonalInfo():
+    pass
+
+
+def inputContactInfo():
+    pass
+
+
+def inputSocialInfo():
+    pass
+
+
+def inputBankInfo():
+    pass
+
+
+def inputCompanyInfo():
+    pass
+
+
+def inputCredentialInfo():
+    pass
