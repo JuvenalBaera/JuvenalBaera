9cdde56c11a1a92638def0636e072870923cdec0
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2023-04-22 10:49:20 +0100

    Add class file

---
+++ python/db_project/database/inputclass.py
@@ -0,0 +1,26 @@
+class Personal:
+    pass
+
+
+class Contact:
+    pass
+
+
+class Address:
+    pass
+
+
+class Social:
+    pass
+
+
+class Bank:
+    pass
+
+
+class Company:
+    pass
+
+
+class Credential:
+    pass
