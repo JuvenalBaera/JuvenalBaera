bc296a5880abf3a96036d44c2399d698c059e189
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2024-03-27 15:18:33 +0000

    class cidadao

--- java/projects/empresa/Main.java
+++ java/projects/empresa/Main.java
@@ -1,10 +1,12 @@
+import cidadao.Cidadao;
 import cidadao.Morada;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         try{
-            Morada m = new Morada("285-513");
-            Morada m1 = new Morada("28513");
+            Morada m = new Morada("rua", 8, "bairro 1", "lisboa", "pt", "2134-234");
+            Cidadao cid = new Cidadao("Juvenal", "Baera", 'M', LocalDate.of(2001, 5, 2), "Mz", false, m);
         }
         catch(Exception ex){
             System.out.println(ex);
--- java/projects/empresa/cidadao/Cidadao.java
+++ java/projects/empresa/cidadao/Cidadao.java
@@ -2,26 +2,25 @@ package cidadao;
 import java.time.LocalDate;
 
 public class Cidadao {
+    private int         id;
+    private char        sexo;
     private String      nome;
     private String      apelido;
-    private char        sexo;
-    private LocalDate   data_nascimento;
-
     private String      local_nascimento;
     private boolean     casado;
-    private int         id;
+    private LocalDate   data_nascimento;
+    private Morada      endereco;
+
     private static int countId = 0;
 
-    Morada endereco;
-
     public Cidadao(String nome, String apelido, char sexo, LocalDate nasci, String local_nas, boolean casado, Morada endereco) throws Exception{
-        this.id = ++countId;
+        this.setSexo(sexo);
+        this.setDataNascimento(nasci);
         this.nome = nome;
         this.apelido = apelido;
-        setSexo(sexo);
         this.endereco = endereco;
+        this.id = ++countId;
     }
-
 
     // setter
     public void setSexo(char sexo) throws Exception{
@@ -36,5 +35,18 @@ public class Cidadao {
             throw new Exception("Menor de idade");
         }
         this.data_nascimento = nascimento;
+    }
+
+    // Getter
+    public int          getId(){return this.id;}
+    public char         getSexo(){return this.sexo;}
+    public String       getNome(){return this.nome;}
+    public String       getApelido(){return this.apelido;}
+    public String       getLocalNascimento(){return this.local_nascimento;}
+    public Morada       getMorada(){return this.endereco;}
+    public LocalDate    getDataNascimento(){return this.data_nascimento;}
+    public String       getEstadoCivil(){
+        if(casado) return "Casado";
+        return "Solteiro";
     }
 }
